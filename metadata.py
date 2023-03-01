cc346a8becc61477d7f5eb50470a9f5e79e85a7a
Lalit Atrish <lalitatrish@IT001776.local>
2023-03-01 13:39:12 +0530

    updated release.sh

--- metadata.py
+++ metadata.py
@@ -1,3 +1,3 @@
 DESCRIPTION = 'Parallel processing of tasks for vertical scaling'
 LONG_DESCRIPTION = 'A package to provide ability to execute a small program in parallel with the ability to stop the execution in the middle without thinking too much about memory leaks.'
-VERSION = '0.0.4'
+VERSION = '0.0.5'
